ab1c4bfefc9548ceeb0d8b577050020d8d676b45
StealthDev <ultrakill148852@gmail.com>
2026-03-29 12:11:35 +0300

    Refactor HandshakeMessage to use fixed-size arrays

--- src/handshake.rs
+++ src/handshake.rs
@@ -1,17 +1,16 @@
-use x25519_dalek::{EphemeralSecret, PublicKey};
+use x25519_dalek::{EphemeralSecret, PublicKey, SharedSecret};
 use rand::rngs::OsRng;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum HandshakeMessage {
-    ClientHello { public_key: Vec<u8> },
-    ServerHello { public_key: Vec<u8> },
+    ClientHello { public_key: [u8; 32] },
+    ServerHello { public_key: [u8; 32] },
     Ack,
 }
 
 pub struct HandshakeState {
     ephemeral_secret: Option<EphemeralSecret>,
-    remote_public_key: Option<PublicKey>,
     shared_secret: Option<[u8; 32]>,
     is_initiator: bool,
 }
@@ -20,7 +19,6 @@ impl HandshakeState {
     pub fn new(initiator: bool) -> Self {
         HandshakeState {
             ephemeral_secret: None,
-            remote_public_key: None,
             shared_secret: None,
             is_initiator: initiator,
         }
@@ -28,32 +26,34 @@ impl HandshakeState {
 
     pub fn create_client_hello() -> (HandshakeMessage, EphemeralSecret) {
         let ephemeral = EphemeralSecret::random_from_rng(OsRng);
-        let public = ephemeral.diffie_hellman_public_key();
+        let public = PublicKey::from(&ephemeral);
         let msg = HandshakeMessage::ClientHello {
-            public_key: public.as_bytes().to_vec(),
+            public_key: public.to_bytes(),
         };
         (msg, ephemeral)
     }
 
-    pub fn process_client_hello(&mut self, client_public: &[u8]) -> HandshakeMessage {
+    pub fn process_client_hello(&mut self, client_public: [u8; 32]) -> HandshakeMessage {
         let ephemeral = EphemeralSecret::random_from_rng(OsRng);
-        let server_public = ephemeral.diffie_hellman_public_key();
+        let server_public = PublicKey::from(&ephemeral);
         
-        if let Ok(client_pk) = PublicKey::from_bytes(client_public.try_into().unwrap()) {
-            self.shared_secret = Some(*ephemeral.diffie_hellman(&client_pk));
+        if let Ok(client_pk) = PublicKey::try_from(client_public) {
+            let shared: SharedSecret = ephemeral.diffie_hellman(&client_pk);
+            self.shared_secret = Some(shared.to_bytes());
         }
         
         self.ephemeral_secret = Some(ephemeral);
         
         HandshakeMessage::ServerHello {
-            public_key: server_public.as_bytes().to_vec(),
+            public_key: server_public.to_bytes(),
         }
     }
 
-    pub fn process_server_hello(&mut self, server_public: &[u8]) -> bool {
+    pub fn process_server_hello(&mut self, server_public: [u8; 32]) -> bool {
         if let Some(secret) = &self.ephemeral_secret {
-            if let Ok(server_pk) = PublicKey::from_bytes(server_public.try_into().unwrap()) {
-                self.shared_secret = Some(*secret.diffie_hellman(&server_pk));
+            if let Ok(server_pk) = PublicKey::try_from(server_public) {
+                let shared: SharedSecret = secret.diffie_hellman(&server_pk);
+                self.shared_secret = Some(shared.to_bytes());
                 return true;
             }
         }
