b3b4f886aacfb80f31e2143958c1d4cde134adfc
StealthDev <ultrakill148852@gmail.com>
2026-03-29 21:45:04 +0300

    Add shared key setup for integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,7 +3,10 @@ use tokio::time::{sleep, Duration};
 
 #[tokio::test]
 async fn test_client_server_basic() {
+    let shared_key = hex::decode("0000000000000000000000000000000000000000000000000000000000000001").unwrap();
+    
     let mut server = VCLConnection::bind("127.0.0.1:9001").await.unwrap();
+    server.set_shared_key(&shared_key);
     
     let server_handle = tokio::spawn(async move {
         server.accept_handshake().await.unwrap();
@@ -14,6 +17,7 @@ async fn test_client_server_basic() {
     });
     
     let mut client = VCLConnection::bind("127.0.0.1:0").await.unwrap();
+    client.set_shared_key(&shared_key);
     client.connect("127.0.0.1:9001").await.unwrap();
     
     for i in 1..=3 {
@@ -25,7 +29,10 @@ async fn test_client_server_basic() {
 
 #[tokio::test]
 async fn test_encryption_integrity() {
+    let shared_key = hex::decode("0000000000000000000000000000000000000000000000000000000000000002").unwrap();
+    
     let mut server = VCLConnection::bind("127.0.0.1:9002").await.unwrap();
+    server.set_shared_key(&shared_key);
     
     let server_handle = tokio::spawn(async move {
         server.accept_handshake().await.unwrap();
@@ -34,6 +41,7 @@ async fn test_encryption_integrity() {
     });
     
     let mut client = VCLConnection::bind("127.0.0.1:0").await.unwrap();
+    client.set_shared_key(&shared_key);
     client.connect("127.0.0.1:9002").await.unwrap();
     client.send(b"secret").await.unwrap();
     server_handle.await.unwrap();
@@ -41,7 +49,10 @@ async fn test_encryption_integrity() {
 
 #[tokio::test]
 async fn test_chain_validation() {
+    let shared_key = hex::decode("0000000000000000000000000000000000000000000000000000000000000003").unwrap();
+    
     let mut server = VCLConnection::bind("127.0.0.1:9003").await.unwrap();
+    server.set_shared_key(&shared_key);
     
     let server_handle = tokio::spawn(async move {
         server.accept_handshake().await.unwrap();
@@ -52,6 +63,7 @@ async fn test_chain_validation() {
     });
     
     let mut client = VCLConnection::bind("127.0.0.1:0").await.unwrap();
+    client.set_shared_key(&shared_key);
     client.connect("127.0.0.1:9003").await.unwrap();
     client.send(b"first").await.unwrap();
     client.send(b"second").await.unwrap();
