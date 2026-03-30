f877b23d92509d11fd4976810a98a1171225fffc
StealthDev <ultrakill148852@gmail.com>
2026-03-30 12:50:11 +0300

    Create client.rs

---
+++ examples/client.rs
@@ -0,0 +1,19 @@
+use vcl_protocol::connection::VCLConnection;
+
+#[tokio::main]
+async fn main() {
+    let mut client = VCLConnection::bind("127.0.0.1:0").await.unwrap();
+    
+    client.connect("127.0.0.1:8080").await.unwrap();
+    println!("Connected to server!");
+    
+    for i in 1..=5 {
+        let msg = format!("Message {}", i);
+        client.send(msg.as_bytes()).await.unwrap();
+        println!("Sent: {}", msg);
+        tokio::time::sleep(tokio::time::Duration::from_millis(100)).await;
+    }
+    
+    client.close().unwrap();
+    println!("Connection closed");
+}
